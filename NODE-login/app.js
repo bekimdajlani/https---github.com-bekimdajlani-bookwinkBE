35dbe525b7a1fee5ec2297c90d4fe4d3780bfe54
Bekim Dajlani <bekim_dajlani@yahoo.com>
2023-03-19 12:01:16 +0100

    insertint jwt token

--- NODE-login/app.js
+++ NODE-login/app.js
@@ -47,7 +47,7 @@ db.connect((error)=>{
 //DEFINE ROUTES
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`Server startet on port ${port} `);
 });
